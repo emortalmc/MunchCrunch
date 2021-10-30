e177df628d79fd6f7bbcf2eae85f88f6190339eb
GoldenCookieGIT <sashaheichenko@icloud.com>
2021-10-29 22:55:18 -0700

    removed test insert

--- src/main/kotlin/dev/emortal/munchcrunch/database/DatabaseUtil.kt
+++ src/main/kotlin/dev/emortal/munchcrunch/database/DatabaseUtil.kt
@@ -3,14 +3,8 @@ package dev.emortal.munchcrunch.database
 import dev.emortal.munchcrunch.ConfigurationHelper
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
-import net.minestom.server.MinecraftServer
 import java.io.File
-import java.nio.file.Path
-import java.time.Duration
 import java.util.*
-import kotlin.io.path.exists
-import kotlin.io.path.readText
-import kotlin.io.path.writeText
 
 class DatabaseUtil() {
     var mainDB: SQLStorage? = null
@@ -18,12 +12,6 @@ class DatabaseUtil() {
         Config.config = initConfigFile(File("./credentials.json"), Config())
         mainDB = SQLStorage(Config.config)
         mainDB!!.connect()
-        mainDB!!.insertIntoTable(
-            "testTable", listOf("UUID", "Gems"),
-            Values(UUID.randomUUID().toString().replace("-", ""),21),
-            Values(UUID.randomUUID().toString().replace("-", ""),13),
-            Values(UUID.randomUUID().toString().replace("-", ""),16)
-        )
     }
 
     private inline fun <reified T : Any> initConfigFile(file: File, emptyObj: T): T {
